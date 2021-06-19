6fc0258a3342166a8fbe6f5d6446ca70ee6444bd
lubigsis <lucre.baluc@gmail.com>
2021-06-19 11:28:07 -0300

    aplico algunos estilos css, subo imagenes

---
+++ script.js
@@ -0,0 +1,101 @@
+/*document.addEventListener('DOMContentLoaded', () => {
+    let cardArray = [
+        {
+            name:'Squirtle',
+            img:'poke1.png'
+        },
+        {
+            name:'Jigglypuff',
+            img:'poke2.png'
+        },
+        {
+            name:'Charmander',
+            img:'poke3.png'
+        },
+        {
+            name:'Bulbasaur',
+            img:'poke4.png'
+        },
+        {
+            name:'Pikachu',
+            img:'poke5.png'
+        },
+        {
+            name:'Caterpie',
+            img:'poke6.png'
+        },
+        {
+            name:'Piplup',
+            img:'poke7.png'
+        },
+        {
+            name:'Marill',
+            img:'poke8.png'
+        },
+        {
+            name:'Psyduck',
+            img:'poke9.png'
+        },
+        {
+            name:'Eevee',
+            img:'poke10.png'
+        },
+
+
+
+        {
+            name:'Squirtle',
+            img:'poke1.png'
+        },
+        {
+            name:'Jigglypuff',
+            img:'poke2.png'
+        },
+        {
+            name:'Charmander',
+            img:'poke3.png'
+        },
+        {
+            name:'Bulbasaur',
+            img:'poke4.png'
+        },
+        {
+            name:'Pikachu',
+            img:'poke5.png'
+        },
+        {
+            name:'Caterpie',
+            img:'poke6.png'
+        },
+        {
+            name:'Piplup',
+            img:'poke7.png'
+        },
+        {
+            name:'Marill',
+            img:'poke8.png'
+        },
+        {
+            name:'Psyduck',
+            img:'poke9.png'
+        },
+        {
+            name:'Eevee',
+            img:'poke10.png'
+        }
+    ]
+
+    let grid= document.querySelector('.content-blocks')
+
+    function createBoard(){
+        for(let i=0; i<cardArray.length;i++){
+            let card= document.createElement('img')
+            card.setAttribute('src','backCard.png')
+            card.setAttribute('data-id', i)
+            //card.addEventListener('click', flipCard)
+            grid.appendChild(card)
+        }
+    }
+
+        createBoard()
+})*/
